5d3a2aa13d62062c6163f94c5cff041b4724904d
Ben Youssef Khalil <benyou_k@etna-alternace.net>
2023-05-08 18:30:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# LeagueOfLegends
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
+
+## dont forget to install the node packages
+
+run `npm install`
+
+## RIOT DEVELOPPERS API KEY
+
+API KEY : RGAPI-676eb8fd-5091-447f-896a-a4ead407afe0
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiAllChampionsURL:
+    'http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json',
+  apiFreeChampionsURL:
+    'https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations',
+  apiImages: 'https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/',
+  apiFreeChampionsKey: 'RGAPI-676eb8fd-5091-447f-896a-a4ead407afe0',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
